6ed4f5c638a952c12afb41319a6158a01bc7e236
11173235 <112845567+11173235@users.noreply.github.com>
2025-12-08 00:05:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 charImages = {
     "奈芙爾": "https://upload-os-bbs.hoyolab.com/upload/2025/10/22/248396204/03efcd616004083b56f1236291a8168c_1765848458562635549.jpg",
-    "甘雨": "https://example.com/images/ganyu.png",
+    "菲林斯": "https://upload-os-bbs.hoyolab.com/upload/2025/09/29/248396204/86565129968b752c57f54294a09f2274_8369161948052686452.jpg",
 }
 
 @app.route("/", methods=["POST"])
 def webhook():
     data = request.get_json()
-    character = data.get("character")  # 從你的聊天機器人接收角色名
+    character = data["queryResult"]["parameters"].get("GenshinCharacter")  # 從你的聊天機器人接收角色名
     image_url = charImages.get(character, "")
-    return jsonify({"message": f"{character} 的培養圖", "image_url": image_url})
+    return jsonify({"message": f"{character} 的培養攻略", "image_url": image_url})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
